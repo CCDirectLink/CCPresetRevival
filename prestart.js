f36bac4433ff7d61128393cc77e7d6f3e9c54739
2767mr <2767mr@users.noreply.github.com>
2021-03-08 09:51:11 +0100

    Delete prestart.js

--- prestart.js
+++
@@ -1,9 +0,0 @@
-sc.TitleScreenButtonGui.inject({
-	init() {
-		this.parent();
-		this._createButton("preset", 175, 6, () => {
-			this.background.doStateTransition("DEFAULT");
-			this.presetMenu.activate();
-		}, "preset");
-	}
-});
