f482b13e52c6d83be3b1d48564f433888de906e9
Alyxia Sother <alyxia@riseup.net>
2023-06-25 10:25:49 +0200

    Format code

--- plugin.js
+++ plugin.js
@@ -1,29 +1,44 @@
 export default class CCPresetRevival {
-    prestart() {
-		sc.TitleScreenButtonGui.inject({
-			init() {
-				this.parent();
-				this._createButton("preset", this.buttons.last().hook.pos.y + 39, this.buttons.length, () => {
-					this.background.doStateTransition("DEFAULT");
-					this.presetMenu.activate();
-				}, "preset");
-			}
-		});
+  prestart() {
+    sc.TitleScreenButtonGui.inject({
+      init() {
+        this.parent();
+        this._createButton(
+          'preset',
+          this.buttons.last().hook.pos.y + 39,
+          this.buttons.length,
+          () => {
+            this.background.doStateTransition('DEFAULT');
+            this.presetMenu.activate();
+          },
+          'preset',
+        );
+      },
+    });
 
+    const oldNames = [
+      '0-before-boss',
+      '1-rhombus-dng-start',
+      '2-continue-story',
+      '3-autumn-rise',
+      '4-apollo-duel',
+      '5-before-bergen',
+      '6-before-maroon',
+      '7-fajro-temple',
+      '8-autumns-fall',
+    ];
 
-		const oldNames = ["0-before-boss", "1-rhombus-dng-start", "2-continue-story", "3-autumn-rise", "4-apollo-duel", "5-before-bergen", "6-before-maroon", "7-fajro-temple", "8-autumns-fall"];
+    //sc.savePreset.slots = [];
 
-		//sc.savePreset.slots = [];
+    const fs = require('fs');
+    const files = fs.readdirSync('assets/data/save-presets/');
+    for (const file of files) {
+      const name = file.replace('.json', '');
+      if (oldNames.includes(name)) {
+        continue;
+      }
 
-		const fs = require('fs');
-		const files = fs.readdirSync('assets/data/save-presets/')
-		for (const file of files) {
-			const name = file.replace('.json', '');
-			if (oldNames.includes(name)) {
-				continue;
-			}
-
-			sc.savePreset.slots.push(new sc.SavePresetData(name));
-		}
+      sc.savePreset.slots.push(new sc.SavePresetData(name));
     }
+  }
 }
